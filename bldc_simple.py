3fc19ce0de5a2c0b19c46da30aebfb357359918b
Faye Terpsichore <157529887+FayeTerpsichore@users.noreply.github.com>
2024-02-06 12:14:34 -0600

    Split controller and simulation apart

--- bldc_simple.py
+++ bldc_simple.py
@@ -1,6 +1,3 @@
-import scipy
-import scipy.integrate
-import scipy.constants
 import scipy.optimize
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,185 +5,7 @@ import matplotlib.animation
 import seaborn as sns
 import sympy
 import typing
-
-
-class Coil:
-    """
-    Represents a coil of wire in the motor.
-    """
-
-    def __init__(
-        self: "Coil",
-        x: float,
-        y: float,
-        z: float,
-        n_windings: int,
-        current: float,
-        resistance: float,
-        theta: float,
-        winding_radius: float,
-        winding_depth: float,
-    ):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.n_windings = n_windings
-        self.current = current
-        self.resistance = resistance
-        self.rotation_matrix = np.array(
-            [
-                [np.cos(theta), -np.sin(theta), 0],
-                [np.sin(theta), np.cos(theta), 0],
-                [0, 0, 1],
-            ]
-        )
-        self.theta = theta
-        self.winding_radius = winding_radius
-        self.winding_depth = winding_depth
-
-    def _l(self, t: float):
-        return self.rotation_matrix @ np.array(
-            [
-                self.x
-                + self.winding_radius
-                * np.cos(2 * np.pi * self.n_windings * t / self.winding_depth),
-                self.y + t,
-                self.x
-                + self.winding_radius
-                * np.sin(2 * np.pi * self.n_windings * t / self.winding_depth),
-            ]
-        )
-
-    def _l_dot(self, t: float):
-        return self.rotation_matrix @ np.array(
-            [
-                -2
-                * np.pi
-                * self.n_windings
-                * self.winding_radius
-                * np.sin(2 * np.pi * self.n_windings * t / self.winding_depth)
-                / self.winding_depth,
-                1,
-                2
-                * np.pi
-                * self.n_windings
-                * self.winding_radius
-                * np.cos(2 * np.pi * self.n_windings * t / self.winding_depth)
-                / self.winding_depth,
-            ]
-        )
-
-    def B_field(self, x: float, y: float, z: float):
-        """
-        Uses the Biot-Savart law to calculate the magnetic field due to this
-        coil at a point (x, y, z).
-        """
-        p = np.array([x, y, z])
-        return (
-            self.current
-            * scipy.constants.mu_0
-            * scipy.integrate.quad_vec(
-                lambda t: np.cross(self._l_dot(t), p - self._l(t))
-                / np.linalg.norm(self._l(t) - p) ** 3,
-                0,
-                self.winding_depth,
-            )[0]
-            / (4 * np.pi)
-        )
-
-
-class Motor:
-    """
-    Represents a simplified model of a three-phase brushless DC motor with the
-    rotor centered on the origin and the stator composed of coils of wire
-    surrounding it embedded in the x-y plane.
-    """
-
-    def __init__(
-        self: "Motor",
-        coils_per_phase: int,
-        inner_radius: float,
-        rotor_magnetic_moment: float,
-        n_windings: int,
-        max_current: float,
-        coil_resistance: float,
-        rotor_moment_of_inertia: float,
-    ):
-        max_unidirectional_B = Coil(
-            0,
-            inner_radius,
-            0,
-            n_windings,
-            max_current,
-            coil_resistance,
-            0,
-            0.5e-2,
-            1e-2,
-        ).B_field(0, 0, 0)
-        rotation_matrix = np.array(
-            [
-                [
-                    np.cos(2 * np.pi / (3 * coils_per_phase)),
-                    -np.sin(2 * np.pi / (3 * coils_per_phase)),
-                    0,
-                ],
-                [
-                    np.sin(2 * np.pi / (3 * coils_per_phase)),
-                    np.cos(2 * np.pi / (3 * coils_per_phase)),
-                    0,
-                ],
-                [0, 0, 1],
-            ]
-        )
-        self.max_Bs = [
-            np.linalg.matrix_power(rotation_matrix, i) @ max_unidirectional_B
-            for i in range(3 * coils_per_phase)
-        ]
-        self.coils_per_phase = coils_per_phase
-        self.coil_resistance = coil_resistance
-        self.rotor_magnetic_moment = rotor_magnetic_moment
-        self.rotor_moment_of_inertia = rotor_moment_of_inertia
-        self.t = 0
-        self.states = []
-        self.currents = []
-        self.controller: "SpeedController" | "PositionController" = None
-
-    def _rhs(self: "Motor", t: float, state: np.array) -> np.array:
-        """
-        RHS of the internal ODE system.
-        """
-        dipole_moment = (
-            np.array([np.cos(state[0]), np.sin(state[0]), 0])
-            * self.rotor_magnetic_moment
-        )
-        input_ = self.controller.control(t, state) / (
-            self.coils_per_phase * self.coil_resistance
-        )
-        B = sum(
-            [
-                B_at_origin * input_[i // self.coils_per_phase]
-                for i, B_at_origin in enumerate(self.max_Bs)
-            ]
-        )
-        torque = np.cross(dipole_moment, B) - 0.01 * state[1]  # added friction term
-        return np.array([state[1], torque[2] / self.rotor_moment_of_inertia])
-
-    def solve(self: "Motor", t_max: float, dt: float):
-        """
-        Solve the ODE system.
-        """
-        if self.controller is None:
-            raise ValueError("Controller not set.")
-        solver = scipy.integrate.ode(self._rhs)
-        solver.set_integrator("vode")
-        solver.set_initial_value(
-            self.states[-1] if self.states else np.zeros(2), self.t
-        )
-        while solver.successful() and solver.t < t_max:
-            solver.integrate(solver.t + dt)
-            self.controller.target_position = np.sqrt(200 * solver.t)
-            self.states.append(solver.y)
-        self.t = t_max
+from bldc_simulation import *
 
 
 class SpeedController:
@@ -207,21 +26,27 @@ class SpeedController:
         self.rotor_moment_of_inertia = motor.rotor_moment_of_inertia
         self.target_speed = target_speed
         self.last_phases = np.zeros(2)
-        self.torque_optimizer = self.find_torque_optimizer()
+        self.torque_finder = self.find_torque()
         self.past_error = 0
-        self.gain = 10
+        self.gain = 5
         self.motor = motor
+        self.last_params = np.zeros(4)
+        self.psis = []
+        self.torques = []
 
-    def find_torque_optimizer(
+    def find_torque(
         self: "SpeedController",
     ) -> typing.Callable[[float, float, float, float], float]:
         """
         Derives the expression relating phase 2's voltage and phase 1's voltage for a
         given torque, rotor angle, and rotor angular velocity.
         """
-        phi_1, phi_2 = sympy.symbols("phi_1 phi_2", real=True)
-        phi_3 = -phi_1 - phi_2
+        psi_1, psi_2 = sympy.symbols("psi_1 psi_2", real=True)
+        alpha_1, alpha_2 = sympy.symbols("alpha_1 alpha_2", real=True)
         t = sympy.Symbol("t", positive=True, real=True)
+        phi_1 = sympy.sin(2 * sympy.pi * (t * self.target_speed + psi_1)) * alpha_1
+        phi_2 = sympy.sin(2 * sympy.pi * (t * self.target_speed + psi_2)) * alpha_2
+        phi_3 = -phi_1 - phi_2
         theta = sympy.Function("theta")(t)
         dipole_moment = self.rotor_magnetic_moment * sympy.Matrix(
             [[sympy.cos(theta)], [sympy.sin(theta)], [0]]
@@ -233,11 +58,11 @@ class SpeedController:
         for i, B_at_origin in enumerate(self.max_Bs):
             B += sympy.Matrix(B_at_origin) * input_[i // self.motor_coils_per_phase]
         B = B.transpose()
-        torque = dipole_moment.cross(B) - 0.01 * theta.diff(t) * sympy.Matrix([1, 1, 1])
-        tau = sympy.Symbol("tau", real=True)
-        # print(sympy.simplify(torque[2]))
+        torque = dipole_moment.cross(B) - 0.01 * theta.diff(t) * sympy.Matrix(
+            [[1], [1], [1]]
+        )
         return sympy.lambdify(
-            (tau, phi_2, theta, theta.diff(t)), sympy.solve(torque[2] - tau, phi_1)[0]
+            (t, alpha_1, alpha_2, psi_1, psi_2, theta, theta.diff(t)), torque[2]
         )
 
     def control(self: "SpeedController", t: float, state: np.array) -> np.array:
@@ -246,23 +71,45 @@ class SpeedController:
         """
         error = self.target_speed - state[1]
         desired_torque = error * self.rotor_moment_of_inertia * self.gain
-        phase_2 = scipy.optimize.minimize(
-            lambda phases: np.linalg.norm(
-                [
-                    self.torque_optimizer(
-                        desired_torque, phases[1], state[0], state[1]
-                    ),
-                    phases[0],
-                ]
-            ),
-            self.last_phases,
-            bounds=[(-15, 15)] * 2,
+        alpha_1 = 7.5
+        alpha_2 = 7.5
+        psi_1, psi_2 = scipy.optimize.minimize(
+            lambda x: (
+                self.torque_finder(t, alpha_1, alpha_2, x[0], x[1], state[0], state[1])
+                - desired_torque
+            )
+            ** 2,
+            np.zeros(2),
+            bounds=[(-np.pi, np.pi)] * 2,
             method="L-BFGS-B",
-            options={"ftol": 1e-2},
-        ).x[1]
-        phase_1 = self.torque_optimizer(desired_torque, phase_2, state[0], state[1])
-        self.last_phases = np.array([phase_1, phase_2])
-        return np.array([phase_1, phase_2, -phase_1 - phase_2])
+            options={"ftol": 1e-14},
+        ).x
+        print(t)
+        phase_1 = alpha_1 * np.sin(2 * np.pi * (t * self.target_speed + psi_1))
+        phase_2 = alpha_2 * np.sin(2 * np.pi * (t * self.target_speed + psi_2))
+        phase_3 = -phase_1 - phase_2
+        self.torques.append(
+            self.torque_finder(t, alpha_1, alpha_2, psi_1, psi_2, state[0], state[1])
+        )
+        # print(self.torque_finder(desired_torque, phase_2, state[0], state[1]))
+        # phase_2 = scipy.optimize.minimize(
+        #     lambda phases: np.linalg.norm(
+        #         [
+        #             self.torque_optimizer(
+        #                 desired_torque, phases[1], state[0], state[1]
+        #             ),
+        #             phases[0],
+        #         ]
+        #     ),
+        #     self.last_phases,
+        #     bounds=[(-15, 15)] * 2,
+        #     method="L-BFGS-B",
+        #     options={"ftol": 1e-2},
+        # ).x[1]
+        # phase_1 = self.torque_optimizer(desired_torque, phase_2, state[0], state[1])
+        self.last_params = np.array([alpha_1, alpha_2, psi_1, psi_2])
+        self.psis.append(np.array([psi_1, psi_2]))
+        return np.array([phase_1, phase_2, phase_3])
 
 
 class PositionController:
@@ -284,8 +131,8 @@ class PositionController:
 
 
 motor = Motor(3, 1e-2, 7e-2, 2000, 1, 0.1, 1e-3)
-# motor.controller = SpeedController(motor, -10)
-motor.controller = PositionController(motor, np.pi - 0.00000000001)
+motor.controller = SpeedController(motor, -10)
+# motor.controller = PositionController(motor, np.pi - 0.00000000001)
 
 motor.solve(10, 1 / 60)
 
@@ -298,6 +145,13 @@ sns.set(
     font_scale=1.5,
     rc={"lines.linewidth": 2, "grid.linestyle": "--"},
 )
+
+ax.clear()
+ax.set_title("Control signals")
+ax.set_xlabel("timestep")
+ax.set_ylabel("torque (N-m)")
+ax.plot(motor.controller.torques)
+fig.savefig("bldc_simple_torques.png")
 
 
 def animate(i):
---
+++ bldc_simulation.py
@@ -0,0 +1,181 @@
+import numpy as np
+import scipy.constants
+import scipy.integrate
+
+
+class Coil:
+    """
+    Represents a coil of wire in the motor.
+    """
+
+    def __init__(
+        self: "Coil",
+        x: float,
+        y: float,
+        z: float,
+        n_windings: int,
+        current: float,
+        resistance: float,
+        theta: float,
+        winding_radius: float,
+        winding_depth: float,
+    ):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.n_windings = n_windings
+        self.current = current
+        self.resistance = resistance
+        self.rotation_matrix = np.array(
+            [
+                [np.cos(theta), -np.sin(theta), 0],
+                [np.sin(theta), np.cos(theta), 0],
+                [0, 0, 1],
+            ]
+        )
+        self.theta = theta
+        self.winding_radius = winding_radius
+        self.winding_depth = winding_depth
+
+    def _l(self, t: float):
+        return self.rotation_matrix @ np.array(
+            [
+                self.x
+                + self.winding_radius
+                * np.cos(2 * np.pi * self.n_windings * t / self.winding_depth),
+                self.y + t,
+                self.x
+                + self.winding_radius
+                * np.sin(2 * np.pi * self.n_windings * t / self.winding_depth),
+            ]
+        )
+
+    def _l_dot(self, t: float):
+        return self.rotation_matrix @ np.array(
+            [
+                -2
+                * np.pi
+                * self.n_windings
+                * self.winding_radius
+                * np.sin(2 * np.pi * self.n_windings * t / self.winding_depth)
+                / self.winding_depth,
+                1,
+                2
+                * np.pi
+                * self.n_windings
+                * self.winding_radius
+                * np.cos(2 * np.pi * self.n_windings * t / self.winding_depth)
+                / self.winding_depth,
+            ]
+        )
+
+    def B_field(self, x: float, y: float, z: float):
+        """
+        Uses the Biot-Savart law to calculate the magnetic field due to this
+        coil at a point (x, y, z).
+        """
+        p = np.array([x, y, z])
+        return (
+            self.current
+            * scipy.constants.mu_0
+            * scipy.integrate.quad_vec(
+                lambda t: np.cross(self._l_dot(t), p - self._l(t))
+                / np.linalg.norm(self._l(t) - p) ** 3,
+                0,
+                self.winding_depth,
+            )[0]
+            / (4 * np.pi)
+        )
+
+
+class Motor:
+    """
+    Represents a simplified model of a three-phase brushless DC motor with the
+    rotor centered on the origin and the stator composed of coils of wire
+    surrounding it embedded in the x-y plane.
+    """
+
+    def __init__(
+        self: "Motor",
+        coils_per_phase: int,
+        inner_radius: float,
+        rotor_magnetic_moment: float,
+        n_windings: int,
+        max_current: float,
+        coil_resistance: float,
+        rotor_moment_of_inertia: float,
+    ):
+        max_unidirectional_B = Coil(
+            0,
+            inner_radius,
+            0,
+            n_windings,
+            max_current,
+            coil_resistance,
+            0,
+            0.5e-2,
+            1e-2,
+        ).B_field(0, 0, 0)
+        rotation_matrix = np.array(
+            [
+                [
+                    np.cos(2 * np.pi / (3 * coils_per_phase)),
+                    -np.sin(2 * np.pi / (3 * coils_per_phase)),
+                    0,
+                ],
+                [
+                    np.sin(2 * np.pi / (3 * coils_per_phase)),
+                    np.cos(2 * np.pi / (3 * coils_per_phase)),
+                    0,
+                ],
+                [0, 0, 1],
+            ]
+        )
+        self.max_Bs = [
+            np.linalg.matrix_power(rotation_matrix, i) @ max_unidirectional_B
+            for i in range(3 * coils_per_phase)
+        ]
+        self.coils_per_phase = coils_per_phase
+        self.coil_resistance = coil_resistance
+        self.rotor_magnetic_moment = rotor_magnetic_moment
+        self.rotor_moment_of_inertia = rotor_moment_of_inertia
+        self.t = 0
+        self.states = []
+        self.currents = []
+        self.controller = None
+
+    def _rhs(self: "Motor", t: float, state: np.array) -> np.array:
+        """
+        RHS of the internal ODE system.
+        """
+        dipole_moment = (
+            np.array([np.cos(state[0]), np.sin(state[0]), 0])
+            * self.rotor_magnetic_moment
+        )
+        input_ = self.controller.control(t, state) / (
+            self.coils_per_phase * self.coil_resistance
+        )
+        B = sum(
+            [
+                B_at_origin * input_[i // self.coils_per_phase]
+                for i, B_at_origin in enumerate(self.max_Bs)
+            ]
+        )
+        torque = np.cross(dipole_moment, B) - 0.01 * state[1]  # added friction term
+        return np.array([state[1], torque[2] / self.rotor_moment_of_inertia])
+
+    def solve(self: "Motor", t_max: float, dt: float):
+        """
+        Solve the ODE system.
+        """
+        if self.controller is None:
+            raise ValueError("Controller not set.")
+        solver = scipy.integrate.ode(self._rhs)
+        solver.set_integrator("vode", method="bdf", order=5)
+        solver.set_initial_value(
+            self.states[-1] if self.states else np.zeros(2), self.t
+        )
+        while solver.successful() and solver.t < t_max:
+            solver.integrate(solver.t + dt)
+            self.states.append(solver.y)
+        self.t = t_max
